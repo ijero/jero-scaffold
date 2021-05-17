64a5aa9fd150d915aafe99423fab393ff9461faa
WangChun <ijerocn@qq.com>
2021-05-17 11:28:39 +0800

    优化dp和sp的获取

--- scaffold-ext/src/main/java/cn/ijero/scaffold/ext/DimensionExt.kt
+++ scaffold-ext/src/main/java/cn/ijero/scaffold/ext/DimensionExt.kt
@@ -3,40 +3,16 @@ package cn.ijero.scaffold.ext
 import android.content.res.Resources
 import android.util.TypedValue
 
-val Int.dpInt: Int
+val Number.dpInt: Int
     get() = dp(this.toFloat()).toInt()
 
-val Float.dpInt: Int
-    get() = dp(this).toInt()
-
-val Double.dpInt: Int
-    get() = dp(this.toFloat()).toInt()
-
-val Int.spInt: Int
+val Number.spInt: Int
     get() = sp(this.toFloat()).toInt()
 
-val Float.spInt: Int
-    get() = sp(this).toInt()
-
-val Double.spInt: Int
-    get() = sp(this.toFloat()).toInt()
-
-val Int.dp: Float
+val Number.dp: Float
     get() = dp(this.toFloat())
 
-val Float.dp: Float
-    get() = dp(this)
-
-val Double.dp: Float
-    get() = dp(this.toFloat())
-
-val Int.sp: Float
-    get() = sp(this.toFloat())
-
-val Float.sp: Float
-    get() = sp(this)
-
-val Double.sp: Float
+val Number.sp: Float
     get() = sp(this.toFloat())
 
 private fun dp(value: Float): Float {
